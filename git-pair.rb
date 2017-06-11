be96b89be5db03dccd967115f9e5696d5cc67bc3
Sahil Bajaj <sahil29@gmail.com>
2017-06-11 11:55:24 +0800

    Fix git-pair hash

--- git-pair.rb
+++ git-pair.rb
@@ -2,7 +2,7 @@ class GitPair < Formula
   desc "Bash-only git pairing script 🤜🤛"
   homepage "https://github.com/spinningarrow/git-pair"
   url "https://github.com/spinningarrow/git-pair/archive/1.0.0.tar.gz"
-  sha256 "a5b69dd182a1c67fb07b5600a9c7072ac3422f48121b22e02be5a07c864cb6a6"
+  sha256 "d88859a916e2b1b9d42790cc046f8801c9599f760b0d392e4323eacbb9531c27"
   def install
     bin.install "git-pair"
   end
