ca601974b6090b96288a6d9c11714ef4c2c37736
Sahil Bajaj <sahil29@gmail.com>
2017-07-16 03:16:13 +0800

    Update git-pair to 1.0.1

--- git-pair.rb
+++ git-pair.rb
@@ -1,8 +1,8 @@
 class GitPair < Formula
   desc "Bash-only git pairing script 🤜🤛"
   homepage "https://github.com/spinningarrow/git-pair"
-  url "https://github.com/spinningarrow/git-pair/archive/1.0.0.tar.gz"
-  sha256 "d88859a916e2b1b9d42790cc046f8801c9599f760b0d392e4323eacbb9531c27"
+  url "https://github.com/spinningarrow/git-pair/archive/1.0.1.tar.gz"
+  sha256 "ad51b8d767cded42923fdb2217b3f4515a646c2c25c470c760747b6eb87e9ad3"
   def install
     bin.install "git-pair"
   end
